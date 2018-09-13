b9299021c7924465deab9be4345a67c5961673af
Wei Dai <wei.dai.wd278@yale.edu>
2018-09-12 21:41:12 -0400

    linear model function for homework 1

--- R/linear-models.r
+++ R/linear-models.r
@@ -5,11 +5,30 @@
 #' @param formula a formula
 #' @param data a data.frame
 #' @return An lm object
-#' @importFrom stats lm
+#' @importFrom stats svd
 #' @examples
 #' fit <- linear_model(Sepal.Length ~., iris)
 #' summary(fit)
 #' @export
 linear_model <- function(formula, data) {
-  lm(formula, data)
+  var = all.vars(formula)
+  y = data[, var[1]]
+  if(formula == 'y~.'){
+    X = data[, -which(colnames(data) == var[1])]
+    X = cbind(rep(1, nrow(X)), X)
+    colnames(X)[1] = 'Intercept'
+  }else {
+    X = data[, var[-1]]
+    X = cbind(rep(1, nrow(X)), X)
+    colnames(X)[1] = 'Intercept'
+  }
+  
+  X_svd = svd(X)
+  U = X_svd$u
+  V = X_svd$v
+  D_inverse = diag(1 / X_svd$d)
+  beta = V %*% D_inverse %*% t(U) %*% y
+  my_coef = list(coefficients = beta)
+  class(my_coef) = 'lm'
+  return(my_coef)
 }
