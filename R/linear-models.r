3c0f5c8aa2de9f2988795e325341902ec33808d3
Wei Dai <wei.dai.wd278@yale.edu>
2018-09-12 22:11:52 -0400

    linear model function for homework1 with qr decomposition

--- R/linear-models.r
+++ R/linear-models.r
@@ -10,7 +10,23 @@
 #' fit <- linear_model(Sepal.Length ~., iris)
 #' summary(fit)
 #' @export
+
 linear_model <- function(formula, data) {
+  var = all.vars(formula)
+  y = data[, var[1]]
+  X = model.matrix(formula, data)
+  
+  X_QR <- qr(X)
+  beta <- solve.qr(X_QR, y)
+  beta[which((beta - 0) < 1e-8)] = NA
+  
+  my_coef = list(coefficients = beta)
+  class(my_coef) = 'lm'
+  return(my_coef)
+}
+
+linear_model2 <- function(formula, data) {
+  options(digits = 4)
   var = all.vars(formula)
   y = data[, var[1]]
   X = model.matrix(formula, data)
@@ -20,6 +36,7 @@ linear_model <- function(formula, data) {
   V = X_svd$v
   D_inverse = diag(1 / X_svd$d)
   beta = V %*% D_inverse %*% t(U) %*% y
+  beta[which((beta - 0) < 1e-8)] = NA
   
   my_coef = list(coefficients = beta)
   class(my_coef) = 'lm'
