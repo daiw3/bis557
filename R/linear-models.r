4493ae3847ec9dc1ea9ec9b28d55947047d060b8
Wei Dai <wei.dai.wd278@yale.edu>
2018-09-12 23:27:32 -0400

    Linea Modle Function for Homework 1

--- R/linear-models.r
+++ R/linear-models.r
@@ -10,18 +10,29 @@
 #' summary(fit)
 #' @export
 
+# linear_model3 <- function(formula, data){
+#   fit <- lm(formula, data)
+#   return(fit)
+# }
+
 linear_model <- function(formula, data) {
   var = all.vars(formula)
   y = data[, var[1]]
   X = model.matrix(formula, data)
   
-  X_QR <- qr(X)
-  beta <- solve.qr(X_QR, y)
+  X_QR = qr(X)
+  beta = solve.qr(X_QR, y)
   beta[which(beta == 0)] = NA
   
-  my_coef = list(coefficients = beta)
-  class(my_coef) = 'lm'
-  return(my_coef)
+  fitted = X %*% beta
+  residuals = y - fitted
+  linear_model_fit = list(coefficients = beta, residals = residuals, fitted.values = fitted,
+                 rank = ncol(X), weights = NULL, df.residual = nrow(X) - ncol(X), 
+                 call = call('lm', formula), terms = terms(x = formula, data = data),
+                 contrasts = NA, xlevels = NA, offset = NA, y = y, x = X, 
+                 model = formula, na.action = NA, qr = X_QR)
+  class(linear_model_fit) = 'lm'
+  return(linear_model_fit)
 }
 
 linear_model2 <- function(formula, data) {
