3e42324a63fc35cb22aa627feef7e333c72d85f2
Wei Dai <wei.dai.wd278@yale.edu>
2018-12-16 10:41:57 -0500

    Homewook-5

--- R/data-doc.r
+++ R/data-doc.r
@@ -24,4 +24,136 @@ NULL
 #' @description In homework 2 we use this dataset to test the ridge regression model
 NULL
 
+#' A data set for the 5th homework
+#'
+#' @name emnist_train
+#' @docType data
+#' @rdname emnist_train
+#' @description In homework 5, this is the training dataset for EMNIST.
+NULL
+
+#' A data set for the 5th homework
+#'
+#' @name emnist_test
+#' @docType data
+#' @rdname emnist_test
+#' @description In homework 5, this is the testing dataset for EMNIST.
+NULL
+
+
+
+#' A data set for the 5th homework
+#'
+#' @name predict_train
+#' @docType data
+#' @rdname predict_train
+#' @description In homework 5, this is the predicted value for problem2.
+NULL
+
+#' A data set for the 5th homework
+#'
+#' @name predict.test
+#' @docType data
+#' @rdname predict_test
+#' @description In homework 5, this is the predicted value for problem2.
+NULL
+
+#' A data set for the 5th homework
+#'
+#' @name predict.train
+#' @docType data
+#' @rdname predict_train
+#' @description In homework 5, this is the predicted value for problem2.
+NULL
+
+#' A data set for the 5th homework
+#'
+#' @name predict_test
+#' @docType data
+#' @rdname predict_test
+#' @description In homework 5, this is the predicted value for problem2.
+NULL
+
+
+#' A data set for the 5th homework
+#'
+#' @name history
+#' @docType data
+#' @rdname history
+#' @description In homework 5, this is the history of training model.
+NULL
+
+
+#' A data set for the 5th homework
+#'
+#' @name predict_train_1
+#' @docType data
+#' @rdname predict_train_1
+#' @description In homework 5, this is the predicted value for problem1.
+NULL
+
+#' A data set for the 5th homework
+#'
+#' @name predict.test.1
+#' @docType data
+#' @rdname predict_test_1
+#' @description In homework 5, this is the predicted value for problem1.
+NULL
+
+#' A data set for the 5th homework
+#'
+#' @name predict.train.1
+#' @docType data
+#' @rdname predict_train_1
+#' @description In homework 5, this is the predicted value for problem1.
+NULL
+
+#' A data set for the 5th homework
+#'
+#' @name predict_test_1
+#' @docType data
+#' @rdname predict_test_1
+#' @description In homework 5, this is the predicted value for problem1.
+NULL
+
+
+#' A data set for the 5th homework
+#'
+#' @name history1
+#' @docType data
+#' @rdname history1
+#' @description In homework 5, this is the history of training model in problem 1.
+NULL
+
+#' A data set for the 5th homework
+#'
+#' @name y_train
+#' @docType data
+#' @rdname y_train
+#' @description In homework 5, this is the y value of training model in problem 2.
+NULL
+
+#' A data set for the 5th homework
+#'
+#' @name y_test
+#' @docType data
+#' @rdname y_test
+#' @description In homework 5, this is the y value of testing model in problem 2.
+NULL
+
+#' A data set for the 5th homework
+#'
+#' @name y.train
+#' @docType data
+#' @rdname y.train
+#' @description In homework 5, this is the y value of training model in problem 2.
+NULL
+
+#' A data set for the 5th homework
+#'
+#' @name y.test
+#' @docType data
+#' @rdname y.test
+#' @description In homework 5, this is the y value of testing model in problem 2.
+NULL
 
